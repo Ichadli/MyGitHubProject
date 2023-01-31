2a3ccda85c0fddcdd0a4344f0e4f0d1171a205d9
Ichadli <123709444+Ichadli@users.noreply.github.com>
2023-01-30 21:07:55 -0500

    Update FirstGitHubClass.java
    "Hey" changed to "Hello"

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hey GitHub 🤚.");
+        System.out.println("Hello GitHub 🤚.");
         System.out.println(1+1);
     }
 }
